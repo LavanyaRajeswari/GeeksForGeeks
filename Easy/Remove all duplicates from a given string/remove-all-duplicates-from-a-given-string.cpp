8cacc2f83bdc1a34fa813e6179c86d0a0f5acedd
LavanyaRajeswari <146519974+LavanyaRajeswari@users.noreply.github.com>
2024-03-10 22:35:36 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove all duplicates from a given string/remove-all-duplicates-from-a-given-string.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string str) {
+	    int n=str.size();
+	    unordered_map<char,int>mpp;
+	    string ans = "";
+	    for(int i=0;i<=n;i++){
+	            mpp[str[i]]++;
+	    }
+	    for(int i=str.size();i>=0;i--){
+	        if(mpp[str[i]] > 1) mpp[str[i]]--;
+	        else ans+=str[i];
+	    }
+	    reverse(ans.begin(),ans.end());
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
