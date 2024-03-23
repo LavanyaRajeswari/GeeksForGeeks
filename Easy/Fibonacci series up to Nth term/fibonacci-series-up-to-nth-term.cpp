f9586daa70ed80a5a78b9a64ba76f8fad8e958bf
LavanyaRajeswari <146519974+LavanyaRajeswari@users.noreply.github.com>
2024-03-23 11:38:34 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Fibonacci series up to Nth term/fibonacci-series-up-to-nth-term.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> Series(int n) {
+        vector<int>v(n+1,0);
+        int mod = 1e9+7;
+        v[1] = 1;
+        for(int i=2;i<=n;i++){
+            v[i]=(v[i-1]+v[i-2]) % mod;
+        }
+    	return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution obj;
+
+        vector<int> ans = obj.Series(n);
+        for (auto x : ans) cout << x << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
