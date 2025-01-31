2090d84864251a385caac6cc2655ea25e0b98bb6
LavanyaRajeswari <146519974+LavanyaRajeswari@users.noreply.github.com>
2025-01-31 23:19:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Replace O's with X's/replace-os-with-xs.cpp
+++ Difficulty: Medium/Replace O's with X's/replace-os-with-xs.cpp
@@ -10,31 +10,33 @@ using namespace std;
 
 class Solution {
   public:
-    void changeState(int i,int j,int n, int m, vector<vector<int>>&state, vector<vector<char>>&mat){
-        if (i>=n || j>=m || i<0 || j<0 || mat[i][j] == 'X' || state[i][j]==1) return;
-        state[i][j] = 1;
-        changeState(i+1,j,n, m,state,mat);
-        changeState(i-1,j,n, m,state,mat);
-        changeState(i,j+1,n, m,state,mat);
-        changeState(i,j-1,n, m,state,mat);
+    void rec(vector<vector<char>>& mat,int i,int j,int n,int m) {
+        if(i >= n || j >= m || i < 0 || j < 0 || mat[i][j] != 'O') {
+            return;
+        }
+        mat[i][j] = '$';
+        rec(mat,i+1,j,n,m);
+        rec(mat,i-1,j,n,m);
+        rec(mat,i,j+1,n,m);
+        rec(mat,i,j-1,n,m);
     }
+    
     vector<vector<char>> fill(vector<vector<char>>& mat) {
-        // code here
-        int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> state(n, vector<int> (m, 0));
-        for (int i=0; i<n; i++){
-            if (mat[i][0] == 'O') changeState(i,0,n,m,state,mat);
-            if (mat[i][m-1] == 'O') changeState(i,m-1,n,m,state,mat);
+        int n = mat.size(),m = mat[0].size();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(mat[i][j] == 'O' and i == 0 || j == 0 || i == n-1 || j == m-1) {
+                    rec(mat,i,j,n,m);
+                }
+            }
         }
-        for (int j=0; j<m; j++){
-            if (mat[0][j] == 'O') changeState(0,j,n,m,state,mat);
-            if (mat[n-1][j] == 'O') changeState(n-1,j,n,m,state,mat);
-        }
-        for (int i=0; i<n; i++){
-            for (int j=0; j<m; j++){
-                if (mat[i][j] == 'O'){
-                    if (state[i][j] == 1)continue;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(mat[i][j] == 'O') {
                     mat[i][j] = 'X';
+                }
+                if(mat[i][j] == '$') {
+                    mat[i][j] = 'O';
                 }
             }
         }
