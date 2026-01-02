26398785cd1def4a3dc0b6c2fb6756ace02f97e7
Lavanya <146519974+LavanyaRajeswari@users.noreply.github.com>
2026-01-02 12:15:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -1,15 +1,9 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
-
-// } Driver Code Ends
 class Solution
 {
     public:
-    void sort012(int a[], int n)
+    void sort012(vector<int>&a)
     {
+        int n = a.size();
         map<int,int>mp;
         for(int i = 0; i < n; i++) {
             mp[a[i]]++;
@@ -25,34 +19,3 @@ class Solution
     }
     
 };
-
-//{ Driver Code Starts.
-int main() {
-
-    int t;
-    cin >> t;
-
-    while(t--){
-        int n;
-        cin >>n;
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin >> a[i];
-        }
-
-        Solution ob;
-        ob.sort012(a, n);
-
-        for(int i=0;i<n;i++){
-            cout << a[i]  << " ";
-        }
-
-        cout << endl;
-        
-        
-    }
-    return 0;
-}
-
-
-// } Driver Code Ends
