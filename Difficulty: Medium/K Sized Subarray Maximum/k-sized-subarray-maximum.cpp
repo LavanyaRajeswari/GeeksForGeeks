c9ebb93ad35fc964827eed80a45abd2b9c11ea16
Lavanya <146519974+LavanyaRajeswari@users.noreply.github.com>
2026-01-12 19:02:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,68 +1,29 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-  public:
+public:
     vector<int> maxOfSubarrays(vector<int>& arr, int k) {
-        vector<int>ans;
-        multiset<int>st;
+        vector<int> ans;
+        deque<int> dq;  
+        int n = arr.size();
+        
         for(int i = 0; i < k; i++) {
-            st.insert(arr[i]);
+            while(!dq.empty() && arr[dq.back()] <= arr[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
         }
-        int i = 0, j = k,n = arr.size();
-        ans.push_back(*st.rbegin());
-        while(j < n) {
-            // for(auto it:st) {
-            //     cout<<it<<" ";
-            // }
-            // cout<<endl;
-            st.erase(st.find(arr[i]));
-            st.insert(arr[j]);
-            ans.push_back(*st.rbegin());
-            j++;
-            i++;
+        ans.push_back(arr[dq.front()]);
+        for(int i = k; i < n; i++) {
+            if(!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+            
+            while(!dq.empty() && arr[dq.back()] <= arr[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            
+            ans.push_back(arr[dq.front()]);
         }
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int k;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> k;
-        cin.ignore(); // Ignore newline character after k input
-
-        Solution obj;
-        vector<int> res = obj.maxOfSubarrays(arr, k);
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
