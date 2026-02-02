8bf67fc0ad5426acec550811d5a21602b069a209
Lavanya <146519974+LavanyaRajeswari@users.noreply.github.com>
2026-02-02 18:26:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fibonacci Using Recursion/fibonacci-using-recursion.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int nthFibonacci(int n) {
+           if(n == 0) return 0;
+    if(n == 1) return 1;
+    else{
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
+    }
+    return 0; 
+    }
+};
